ad90f5e7e9a2ce630ad89046f755e795b8ff80ae
parksanghyun <basira@naver.com>
2026-01-17 13:38:32 +0900

    size 테스트

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
-// 메인 JavaScript 파일
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('프로젝트가 시작되었습니다.');
-    
-    // 여기에 코드를 작성하세요
-});
+// active 클래스 추가 함수
+function misstionActive(element) {
+    element.classList.add('active');
+}
